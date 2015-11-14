5c1de58ada2d4b467b33d986397d044b7ec65234
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-11-14 23:22:23 +0100

    db setup and rename app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_tryIt_session'
+Rails.application.config.session_store :cookie_store, key: '_swift-high-performance-website_session'
