89f3fa0853530b9bba685e18bfbb2d95a1cf36aa
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-11-15 10:11:10 +0100

    add js

---
+++ app/assets/javascripts/functions.js
@@ -0,0 +1,14 @@
+
+/*---------------------------
+LeadPages Custom Functions
+----------------------------*/
+
+
+// Must load 3rd party API after data-attributes have been updated.
+
+    /* Twitter */
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+    /* Google plus */
+    !(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
+    /* Facebook */
+    !(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
